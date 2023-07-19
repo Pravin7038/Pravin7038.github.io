794c1d4d11b50dc74308d57f6e0eaf36da068023
Pravin <pravin08pawar@gmail.com>
2023-07-19 18:33:30 +0530

    commit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
 const button1 = document.querySelector('#resume-button-1');
 const button2 = document.querySelector('#resume-button-2');
 
